7b469f61e282b8251821f973a83ca6057f32d99f
sg4700 <shreyagupta4700@gmail.com>
2020-08-06 20:11:22 +0530

    changes to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Post
-from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from django.apps import apps
 
 class PostAdmin(admin.ModelAdmin): 
